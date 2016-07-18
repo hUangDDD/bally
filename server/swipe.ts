5695f4f59fd7c648512b555346bea79345ebc0f5
hUangDDD <hhh.dong@gmail.com>
2016-07-18 11:35:57 +0800

    kk

---
+++ server/swipe.ts
@@ -0,0 +1,28 @@
+///<reference path="typings/tsd.d.ts"/>
+import {res as res_define} from '../client/src/hall/HallRes.ts';
+import * as fs from 'fs'
+var path=require('path').posix;
+
+let files={};
+function walk(base, p) {
+	var realpath=path.join(base, p);
+	var dirList = fs.readdirSync(realpath);
+	dirList.forEach(function(item){	
+		//var filepath = p+"/"+item;
+		var fn=path.join(realpath, item);
+		if (fs.statSync(fn).isDirectory()) return walk(base, path.join(p, item));
+		files[path.join(p, item)]=1;
+	});
+}
+walk(path.join(__dirname,'../../client/dist/'), 'images');
+
+for (let i=0; i<res_define.length; i++) {
+	if (files[res_define[i].src]) files[res_define[i].src]='used';
+	else {
+		console.log('missed', res_define[i].src);
+	}
+}
+
+for (let n in files) {
+	if (files[n]!='used') console.log('unused', n);
+}
